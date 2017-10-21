9653b15c3bf8794172cda44594e5e87249b12365
Arjan Kwakkel <arjan.kwakkel@nslogin.nl>
2017-10-21 16:21:39 +0200

    👊 QR Code scanner integrated, package config adjusted and menu rearranged.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { QRScanner } from '@ionic-native/qr-scanner';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { SettingsPage } from '../pages/settings/settings';
+import { AboutPage } from '../pages/about/about';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    SettingsPage,
+    AboutPage,
     TabsPage
   ],
   imports: [
@@ -27,13 +28,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    SettingsPage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    QRScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private qrScanner: QRScanner) {
+    // Request the permission if it wasn't done already.
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
 
+      if (status.authorized) {
+        // camera permission was granted
+
+        // show camera preview
+        this.qrScanner.show();
+
+        // start scanning
+        let scanSub = this.qrScanner.scan().subscribe((code: string) => {
+          alert('Scanned the following code: ' + code);
+
+        });
+      } else if (status.denied) {
+        console.log('denied');
+        // camera permission was permanently denied
+        // you must use QRScanner.openSettings() method to guide the user to the settings page
+        // then they can grant the permission from there
+      } else {
+        console.log('not permanently denied');
+        // permission was denied, but not permanently. You can ask for permission again at a later time.
+      }
+    })
+    .catch((e: any) => console.log('Error is', e));
   }
-
 }
